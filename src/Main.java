ef888679577fff2eadc78b0deed8402d83890cae
nikholt1 <niklas1997@live.dk>
2025-03-05 09:31:56 +0100

    added change to main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello-git");
+        System.out.println("Hello-git niklas");
         System.out.println("version 32132");
         System.out.println("Hello from pB");
         for (int i = 0; i < 100; i++) {
